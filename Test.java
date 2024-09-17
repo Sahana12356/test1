a0f7d52e63e58efba60782f02d6b67a96567729e
Sahana12356 <122825053+Sahana12356@users.noreply.github.com>
2024-09-17 09:11:14 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+/**
+ * Test
+ */
+public class Test {
+     public static void main(String[] args) {
+        Emp e1=new Emp("Guldu",1120);
+        Emp e2=new Emp("Dinhga",1122);
+        System.out.println(e1.ename+" "+e1.empno);
+        System.out.println(e2.ename +" "+e2.empno);
+     }  
+}
